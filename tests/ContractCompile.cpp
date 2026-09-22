244f84ee843cfb4f7fe3e1459063b5cc55c148cc
Simon Stuart <lakraven@gmail.com>
2026-09-22 10:14:14 +0200

    Validate explicit filesystem binding profile; refs #1

--- tests/ContractCompile.cpp
+++ tests/ContractCompile.cpp
@@ -4,10 +4,20 @@ namespace {
 
     struct ContractBinding final {};
 
+    using ContractFileSystemProfile =
+            ESPressio::Persistence::Arduino::FileSystemBindingProfile<
+                ESPressio::Persistence::RetentionLevel::Restart,
+                ESPressio::Persistence::TextCaseSensitivity::CaseSensitive,
+                ESPressio::Persistence::MediaRemovability::Fixed,
+                254U,
+                254U,
+                0xFFFFFFFFULL
+            >;
+
 
     static_assert([]() consteval {
         ESPressio::Persistence::ValidatePersistenceProvider<
-            ESPressio::Persistence::Arduino::FileSystemStorage<ContractBinding>
+            ESPressio::Persistence::Arduino::FileSystemStorage<ContractBinding, ContractFileSystemProfile>
         >();
         ESPressio::Persistence::ValidatePersistenceProvider<
             ESPressio::Persistence::Arduino::PreferencesKeyValueStorage<ContractBinding>
